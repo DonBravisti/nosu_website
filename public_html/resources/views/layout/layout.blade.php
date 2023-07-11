7e0654f89d3161e095351c45858f8ed681b6760f
Kirill Khaev <kirillkhaev6391@gmail.com>
2023-07-12 00:43:47 +0300

    added profile editing page

--- public_html/resources/views/layout/layout.blade.php
+++ public_html/resources/views/layout/layout.blade.php
@@ -10,101 +10,103 @@
 		<title>Nosu</title>
 	</head>
 	<body>
-		<header>
-			<div class="header-top-container">
-				<div class="header-top">
-					<div class="header-info">
-						<div>
-							<img src="/images/gpsIcon.png" class="header__icon">
-							<a href="#" class="header__item">ул. Церетели, 18, Владикавказ</a>
-						</div>
-						<div>
-							<img src="/images/timeIcon.png" class="header__icon">
-							<a href="#" class="header__item">Пн-пт: 10:00-17:00</a>
-						</div>
-					</div>
-					<div class="header-contacts">
-						<div>
-							<img src="/images/mailIcon.png" class="header__icon">
-							<a href="#" class="header__item">mit_nosu</a>
-						</div>
-						<div>
-							<img src="/images/phoneIcon.png" class="header__icon">
-							<a href="#" class="header__item">+7 (8672) 333-920</a>
-						</div>
-						<div class="language-block">
-							<a href="#" class="header__item header__item-active language">Рус</a>
-							<a href="#" class="header__item header__item-disabled language">Eng</a>
-							<img src="/images/eye.png" class="header__icon eye-icon">
-						</div>
-					</div>
-				</div>
-			</div>
-			<span class="border"></span>
-			<div class="header-main-container">
-				<div class="header-main">
-					<div class="logo-block">
-						<img src="/images/logo.png" class="logo-block__img">
-					</div>
-					<div class="menu">
-						<div class="menu-top_container">
-							<div class="menu-top">
-								<form action="#" method="post" class="menu-search-block">
-									<input type="text" class="search-input" name="search-input">
-									<input type="submit" class="search-btn" value="">
-								</form>
-								<button class="login-btn">
-								<div class="login-btn__icon"></div>
-								<a class="login-btn__text">Войти</a>
-								</button>
+		<div class="container">
+			<header>
+				<div class="header-top-container">
+					<div class="header-top">
+						<div class="header-info">
+							<div>
+								<img src="/images/gpsIcon.png" class="header__icon">
+								<a href="#" class="header__item">ул. Церетели, 18, Владикавказ</a>
+							</div>
+							<div>
+								<img src="/images/timeIcon.png" class="header__icon">
+								<a href="#" class="header__item">Пн-пт: 10:00-17:00</a>
 							</div>
 						</div>
-						<nav class="navbar">
-							<a href="#" class="navbar__item">Абитуриентам</a>
-							<a href="#" class="navbar__item navbar__item-active">Обучающимся</a>
-							<a href="#" class="navbar__item">Сотрудникам</a>
-							<a href="#" class="navbar__item">Наука и образование</a>
-							<a href="#" class="navbar__item">О факультете</a>
-						</nav>
-					</div>
-				</div>
-				<div class="header-main-mobile">
-					<img src="/images/mobileLogo.png" class="mobile__logo">
-					<div class="mobile-header-block">
-						<div class="mobile-search-block">
-							<input type="text" class="mabile-search__input" name="search-input" placeholder="Что будем искать?">
-							<button class="mobile-search-icon"></button>
+						<div class="header-contacts">
+							<div>
+								<img src="/images/mailIcon.png" class="header__icon">
+								<a href="#" class="header__item">mit_nosu</a>
+							</div>
+							<div>
+								<img src="/images/phoneIcon.png" class="header__icon">
+								<a href="#" class="header__item">+7 (8672) 333-920</a>
+							</div>
+							<div class="language-block">
+								<a href="#" class="header__item header__item-active language">Рус</a>
+								<a href="#" class="header__item header__item-disabled language">Eng</a>
+								<img src="/images/eye.png" class="header__icon eye-icon">
+							</div>
 						</div>
-						<img src="/images/burger.png" class="mabile-search__burger">
 					</div>
 				</div>
-			</div>
-		</header>
-		<main>
-			@yield('content')
-		</main>
-		<footer>
-			<div class="soc-networks-block">
-				<a href="#">
-					<div class="soc-network">
-						<img src="/images/vk.png" class="soc-network__img">
-						<p class="soc-network__name">ВКонтакте</p>
+				<span class="border"></span>
+				<div class="header-main-container">
+					<div class="header-main">
+						<div class="logo-block">
+							<img src="/images/logo.png" class="logo-block__img">
+						</div>
+						<div class="menu">
+							<div class="menu-top_container">
+								<div class="menu-top">
+									<form action="#" method="post" class="menu-search-block">
+										<input type="text" class="search-input" name="search-input">
+										<input type="submit" class="search-btn" value="">
+									</form>
+									<button class="login-btn">
+									<div class="login-btn__icon"></div>
+									<a class="login-btn__text">Войти</a>
+									</button>
+								</div>
+							</div>
+							<nav class="navbar">
+								<a href="#" class="navbar__item">Абитуриентам</a>
+								<a href="#" class="navbar__item navbar__item-active">Обучающимся</a>
+								<a href="#" class="navbar__item">Сотрудникам</a>
+								<a href="#" class="navbar__item">Наука и образование</a>
+								<a href="#" class="navbar__item">О факультете</a>
+							</nav>
+						</div>
 					</div>
-				</a>
-				<a href="#">
-					<div class="soc-network">
-						<img src="/images/telegram.png" class="soc-network__img">
-						<p class="soc-network__name">Telegram</p>
+					<div class="header-main-mobile">
+						<img src="/images/mobileLogo.png" class="mobile__logo">
+						<div class="mobile-header-block">
+							<div class="mobile-search-block">
+								<input type="text" class="mabile-search__input" name="search-input" placeholder="Что будем искать?">
+								<button class="mobile-search-icon"></button>
+							</div>
+							<img src="/images/burger.png" class="mabile-search__burger">
+						</div>
 					</div>
-				</a>
-				<a href="#">
-					<div class="soc-network">
-						<img src="/images/youtube.png" class="soc-network__img">
-						<p class="soc-network__name">Youtube</p>
-					</div>
-				</a>
-			</div>
-		</footer>
+				</div>
+			</header>
+			<main>
+				@yield('content')
+			</main>
+			<footer>
+				<div class="soc-networks-block">
+					<a href="#">
+						<div class="soc-network">
+							<img src="/images/vk.png" class="soc-network__img">
+							<p class="soc-network__name">ВКонтакте</p>
+						</div>
+					</a>
+					<a href="#">
+						<div class="soc-network">
+							<img src="/images/telegram.png" class="soc-network__img">
+							<p class="soc-network__name">Telegram</p>
+						</div>
+					</a>
+					<a href="#">
+						<div class="soc-network">
+							<img src="/images/youtube.png" class="soc-network__img">
+							<p class="soc-network__name">Youtube</p>
+						</div>
+					</a>
+				</div>
+			</footer>
+		</div>
 	</div>
 </body>
 </html>
---
+++ public_html/resources/views/partial/navPath.blade.php
@@ -0,0 +1,14 @@
+<div class="nav-path-container">
+	<div class="nav-path">
+		<a href="#" class="nav-path__link not-mobile__link">Главная</a>
+		<span class="nav-path__img not-mobile__link"></span>
+		<a href="#" class="nav-path__link not-mobile__link">Факультет математики и компьютерных наук</a>
+		<span class="nav-path__img not-mobile__link"></span>
+		<a href="#" class="nav-path__link not-mobile__link">Структура</a>
+		<span class="nav-path__img not-mobile__link"></span>
+		<a href="#" class="nav-path__link not-mobile__link">Кафедра прикладной математики и информатики</a>
+		<span class="nav-path__img not-mobile__link"></span>
+		<span class="nav-path__img mobile__link"></span>
+		<a href="#" class="nav-path__link">Басаева Елена Казбековна</a>
+	</div>
+</div>
